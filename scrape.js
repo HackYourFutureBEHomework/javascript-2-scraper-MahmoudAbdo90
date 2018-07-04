dcf5f9ff0ab3387d0240a4f1ab4e15d43b494638
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-07-04 13:38:08 +0200

    the first try

--- scrape.js
+++ scrape.js
@@ -1,4 +1,14 @@
 // Your custom scraping script.
+let table = document.querySelector("table.cast_list");
+
+let actors = table.querySelectorAll("tr.odd", "tr.even");
+
+let i;
+for (i=0; i < actors.length; i++);
+
+
+
+
 
 // Look at https://github.com/HackYourFutureBelgium/JavaScript2/tree/master/Projects/web-scraper
 // for the version we showed in class.
