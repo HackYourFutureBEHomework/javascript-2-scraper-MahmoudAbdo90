56b3a9dc83339f3b1fe82d1deeedfc3855c9b031
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2018-07-05 17:31:52 +0200

    homework week1 scraping

--- scrape.js
+++ scrape.js
@@ -1,12 +1,33 @@
 // Your custom scraping script.
-let table = document.querySelector("table.cast_list");
-
-let actors = table.querySelectorAll("tr.odd", "tr.even");
-
-let i;
-for (i=0; i < actors.length; i++);
+document.addEventListener("keypress", function() {
 
 
+    let table = document.querySelector("table.cast_list");
+    
+    let actors = table.querySelectorAll("tr.odd", "tr.even");
+    console.clear();
+
+    for (let i = 0; i < actors.length; i++){
+        let name = actors[i].querySelector("[itemprop=actor] a span").innerHTML;
+        let $character = actors[i].querySelector("td.character");
+        let character;
+
+        let linkedCharacter = $character.querySelector("a");
+
+        if (linkedCharacter != null) {
+            character = linkedCharacter.innerHTML;
+        } else {
+        let $character = actors[i].querySelector("td.character");
+            character = $character.innerHTML;
+        }
+
+        console.log("Actor: " + name + " Caharacter: " + character);
+    }
+    
+    
+    
+    
+})
 
 
 
